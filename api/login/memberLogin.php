969c4172fdd85e069fa3900cd613781cdcd474a5
JiHeon <jiheon1992@gmail.com>
2025-12-18 03:47:33 +0900

    test

---
+++ api/login/memberLogin.php
@@ -0,0 +1,38 @@
+<?php
+require_once __DIR__ . '/config/accessToken.php';
+
+// $acToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyU2Vzc2lvbiI6IntcImFjY291bnROb1wiOlwidGh4X2FwaV90ZXN0QGdtYWlsLmNvbVwiLFwiYWdlbnRGbGFnXCI6MSxcImNyZWF0ZWREYXRlXCI6MTc2NDMyMTQ4MDg1MixcImxvZ2luRGF0ZVwiOjE3NjU5NjY0OTYwODYsXCJ0aW1lU3RhbXBcIjoxNzY1OTY2NDk2MDg2LFwidHlwZVwiOjIsXCJ1c2VySWRcIjoyMjIyNTM2fSJ9.NktKuIdvyI2ihhY_A6__lmsaugZTUpdtgS6u04NNStg";
+
+// $curl = curl_init();
+
+// curl_setopt_array($curl, array(
+//    CURLOPT_URL => 'https://eximius-vcc-pay-customer-service.siweipay.com/open-api/v1/station/user/query',
+//    CURLOPT_RETURNTRANSFER => true,
+//    CURLOPT_ENCODING => '',
+//    CURLOPT_MAXREDIRS => 10,
+//    CURLOPT_TIMEOUT => 0,
+//    CURLOPT_FOLLOWLOCATION => true,
+//    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//    CURLOPT_CUSTOMREQUEST => 'POST',
+//    CURLOPT_POSTFIELDS =>'{
+//     "accountNo": "kni1993@naver.com",
+//     "pageIndex": 1,
+//     "pageSize": 2
+// }',
+//    CURLOPT_HTTPHEADER => array(
+//       'access_token: '.$acToken,
+//       'clientId: 74c01d46896d48608367e308edf9e7f1',
+//       'nonce: '.$nonce,
+//       'timestamp: '.$timestamp,
+//       'sign: '.$sign,
+//       'Accept-Language: ko-KR',
+//       'Content-Type: application/json'
+//    ),
+// ));
+
+// $response = curl_exec($curl);
+
+// curl_close($curl);
+// echo $response;
+
+?>
