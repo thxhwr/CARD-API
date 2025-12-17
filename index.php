4aa354472630b872732bd9fc225875164ad1d17f
JiHeon <jiheon1992@gmail.com>
2025-12-17 12:50:24 +0900

    test

--- index.php
+++ index.php
@@ -47,34 +47,39 @@ $data = sprintf('clientId=%s&nonce=%s&timestamp=%s',$clientId,$nonce,$timestamp)
 
 $sign = generateSign($data, SECRET_KEY);
 
-$curl = curl_init();
+echo $timestamp;
 
-curl_setopt_array($curl, array(
-   CURLOPT_URL => 'https://eximius-vcc-pay-customer-service.siweipay.com/open-api/v1/oauth/access-token',
-   CURLOPT_RETURNTRANSFER => true,
-   CURLOPT_ENCODING => '',
-   CURLOPT_MAXREDIRS => 10,
-   CURLOPT_TIMEOUT => 0,
-   CURLOPT_FOLLOWLOCATION => true,
-   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-   CURLOPT_CUSTOMREQUEST => 'POST',
-   CURLOPT_POSTFIELDS =>'{
-    "clientId": "74c01d46896d48608367e308edf9e7f1",
-    "clientSecret": "MTc2NDMyNTk4MTU4MkVYSU1JVVNjYjc5Njc2YWJmOTE0MGQ4YWU4YzhiOTE2MzJlMmNkMA=="
-}',
-   CURLOPT_HTTPHEADER => array(
-      'clientId: 74c01d46896d48608367e308edf9e7f1',
-      'nonce: {{$nonce}}',
-      'timestamp: {{$timestamp}}',
-      'sign: {{$sign}}',
-      'Accept-Language: ko-KR',
-      'Content-Type: application/json'
-   ),
-));
+echo $string.uuid;
 
-$response = curl_exec($curl);
+echo $date.anytime|format('yyyyMMddHHmmss');
+// $curl = curl_init();
 
-curl_close($curl);
-echo $response;
+// curl_setopt_array($curl, array(
+//    CURLOPT_URL => 'https://eximius-vcc-pay-customer-service.siweipay.com/open-api/v1/oauth/access-token',
+//    CURLOPT_RETURNTRANSFER => true,
+//    CURLOPT_ENCODING => '',
+//    CURLOPT_MAXREDIRS => 10,
+//    CURLOPT_TIMEOUT => 0,
+//    CURLOPT_FOLLOWLOCATION => true,
+//    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//    CURLOPT_CUSTOMREQUEST => 'POST',
+//    CURLOPT_POSTFIELDS =>'{
+//     "clientId": "74c01d46896d48608367e308edf9e7f1",
+//     "clientSecret": "MTc2NDMyNTk4MTU4MkVYSU1JVVNjYjc5Njc2YWJmOTE0MGQ4YWU4YzhiOTE2MzJlMmNkMA=="
+// }',
+//    CURLOPT_HTTPHEADER => array(
+//       'clientId: 74c01d46896d48608367e308edf9e7f1',
+//       'nonce: {{$nonce}}',
+//       'timestamp: {{$timestamp}}',
+//       'sign: {{$sign}}',
+//       'Accept-Language: ko-KR',
+//       'Content-Type: application/json'
+//    ),
+// ));
+
+// $response = curl_exec($curl);
+
+// curl_close($curl);
+// echo $response;
 
 ?>
