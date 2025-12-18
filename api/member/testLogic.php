ba25961e4c7b5e2e15089e52914afc31fe7ef58d
JiHeon <jiheon1992@gmail.com>
2025-12-18 19:32:08 +0900

    test

--- api/member/testLogic.php
+++ api/member/testLogic.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require_once __DIR__ . '/../../config/bootstrap.php';
+// require_once __DIR__ . '/../../config/bootstrap.php';
 
 try {
     $pdo->beginTransaction();
