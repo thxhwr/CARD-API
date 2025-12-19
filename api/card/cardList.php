7ce0eff5a3fa2a06e3af0a8dafaf62b7438eb65a
JiHeon <jiheon1992@gmail.com>
2025-12-19 12:52:33 +0900

    test

--- api/card/cardList.php
+++ api/card/cardList.php
@@ -15,7 +15,7 @@ curl_setopt_array($curl, array(
    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
    CURLOPT_CUSTOMREQUEST => 'POST',
    CURLOPT_POSTFIELDS =>'{
-    "cardholderId": 2222539,
+    "cardholderId": 4937280063696510,
     "status": "4",
     "cardMode": 1,
     "pageIndex": "1",
