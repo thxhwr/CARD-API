5865569f37e75fd755866eb5292078ec5cf8591a
JiHeon <jiheon1992@gmail.com>
2025-12-24 00:21:02 +0900

    test

--- api/card/cardList.php
+++ api/card/cardList.php
@@ -15,7 +15,7 @@ curl_setopt_array($curl, array(
    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
    CURLOPT_CUSTOMREQUEST => 'POST',
    CURLOPT_POSTFIELDS =>'{
-    "userId": 2222558,
+    "userId": 2222557,
     "orderNo": "5",
     "amount": 1000,
     "remark": "test"
