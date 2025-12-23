d1eeed27e19dc7b230471fb057bba66c400ae869
JiHeon <jiheon1992@gmail.com>
2025-12-23 21:33:17 +0900

    test

--- api/member/testMemberInsert.php
+++ api/member/testMemberInsert.php
@@ -4,14 +4,18 @@ require_once BASE_PATH . '/config/accessToken.php';
 
 $referrerUserId = null;
 
-$referrerAccountNo = trim($_POST['referrerAccountNo'] ?? 'kni1993@naver.com');
-$userId = trim($_POST['userId'] ?? '2222560');
-$accountNo = trim($_POST['accountNo'] ?? 'ksw93152@nate.com');
-$name    = trim($_POST['name'] ?? '최지헌');
-$phone   = trim($_POST['phone'] ?? '01012341234');
-$address = trim($_POST['address'] ?? '28562 충북 청주시 서원구 1순환로 627 빌딩');
+$referrerAccountNo = trim($_POST['referrerAccountNo'] ?? '');
+$userId = trim($_POST['userId'] ?? '');
+$accountNo = trim($_POST['accountNo'] ?? '');
+$name    = trim($_POST['name'] ?? '');
+$phone   = trim($_POST['phone'] ?? '');
+$address = trim($_POST['address'] ?? '');
 
 // $accountNo = 'test' . time() . '@test.com';
+
+if ($userId === '') {
+    jsonResponse(RES_ACCOUNT_REQUIRED, [], 400);
+}
 
 if ($accountNo === '') {
     jsonResponse(RES_ACCOUNT_REQUIRED, [], 400);
