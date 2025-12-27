36214ef03fbc22cbabba947416b459b98335a0dd
JiHeon <jiheon1992@gmail.com>
2025-12-27 13:46:24 +0900

    test

--- api/card/cardList.php
+++ api/card/cardList.php
@@ -16,12 +16,12 @@ curl_setopt_array($curl, array(
    CURLOPT_CUSTOMREQUEST => 'POST',
    CURLOPT_POSTFIELDS =>'{
     "userId": 100979,
-    "orderNo": "4",
+    "orderNo": "152",
     "amount": -1,
     "remark": "test"
 }',
    CURLOPT_HTTPHEADER => array(
-    'access_token: '.$token['AT_ACCESS_TOKEN'],
+    'access_token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyU2Vzc2lvbiI6IntcImFjY291bnROb1wiOlwieWptNzI0QG5hdmVyLmNvbVwiLFwiYWdlbnRGbGFnXCI6MSxcImNyZWF0ZWREYXRlXCI6MTc2NTc4MzYyNzM0OCxcImxvZ2luRGF0ZVwiOjE3NjY4MTA2NzEyODEsXCJ0aW1lU3RhbXBcIjoxNzY2ODEwNjcxMjgxLFwidHlwZVwiOjIsXCJ1c2VySWRcIjoyMjIyNTUyfSJ9.KEPZdVdgSDbD4Tssq3DzelZ75u5BETg6DDTsSXjaePE',
     'clientId: cb6b629375e44c3ca62c621b53659179',
     'nonce: '.$nonce,
     'timestamp: '.$timestamp,
